4651d5c1000ea710a3b4478150961c1dde369585
Ray1007 <b01901007@ntu.edu.tw>
2014-10-09 19:50:39 +0800

    10.9
    add port 3333

--- ChatServer.java
+++ ChatServer.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.logging.*;
 import java.nio.*;
+import java.nio.channels.*;
 //import gui.*;
 
 public class ChatServer{
@@ -29,7 +30,88 @@ public class ChatServer{
 		ChatServer chatServer = new ChatServer();
 		chatServer.start();
 		
-		try(ServerSocket server = new ServerSocket(3333)){
+		ByteBuffer publicBus = ByteBuffer.allocate(4096);
+		ServerSocketChannel serverChannel;
+		Selector selector;
+		try{
+			serverChannel = ServerSocketChannel.open();
+			ServerSocket ss = serverChannel.socket();
+			ss.bind(new InetSocketAddress(3333));
+			serverChannel.configureBlocking(false);
+			selector = Selector.open();
+			serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		} catch(IOException ex){
+			ex.printStackTrace();
+			return;
+		}
+		boolean newMessage = false;
+		while(true){
+			try{ selector.select(); }
+			catch(IOException ex){ ex.printStackTrace(); break; }
+			
+			Set<SelectionKey> readyKeys = selector.selectedKeys();
+			Iterator<SelectionKey> iterator = readyKeys.iterator();
+			int setSize = readyKeys.size();
+
+			while(iterator.hasNext()){
+				//if(readyKeys.size() > 1)
+				//	System.out.println("BB "+readyKeys.size());
+				SelectionKey key = iterator.next();
+				iterator.remove();
+				
+				try{
+					
+					if(key.isAcceptable()){ // serverChannel accepts new connections.
+						ServerSocketChannel server = (ServerSocketChannel) key.channel();
+						SocketChannel client = server.accept();
+						System.out.println("Accepted connection from "+client);
+						client.configureBlocking(false);
+						SelectionKey clientKey = client.register(selector, SelectionKey.OP_WRITE |
+																	  SelectionKey.OP_READ);
+						clientKey.attach(publicBus.duplicate());
+					}
+					if(key.isReadable() && !newMessage){ // get one client's InputStream.
+						SocketChannel client = (SocketChannel) key.channel();
+						ByteBuffer buffer = (ByteBuffer) key.attachment();
+						newMessage = true;
+						publicBus.clear();
+						
+						client.read(buffer);
+						buffer.flip();
+						
+						//client.read(publicBus);
+						System.out.println("BB "+readyKeys.size());
+						System.out.println("readable "+setSize);
+						
+						// breaks while(iterator.hasNext()) loop.
+						break; 
+					}
+					if(key.isWritable() && newMessage){ // write to one client's outputStream.
+						SocketChannel client = (SocketChannel) key.channel();
+						ByteBuffer buffer = (ByteBuffer) key.attachment();
+						
+						client.write(buffer);
+						buffer.flip();
+						System.out.println("writeable "+setSize);
+						setSize--;
+						if(setSize == 0){
+							newMessage = false;
+							//publicBus.clear();
+						}
+					}
+				} catch(IOException ex){
+					System.out.println("some jumpin dog.");
+					setSize--;
+					key.cancel();
+					publicBus.clear();
+					try{
+						key.channel().close();
+					} catch(IOException cex){ cex.printStackTrace(); }
+				}
+			}
+		}
+
+		/*try(ServerSocket server = new ServerSocket(3333)){
 			while(true){
 				try(Socket connection = server.accept()){
 					System.out.println("Port 3333 Accepted connection from "+ connection);
@@ -40,11 +122,11 @@ public class ChatServer{
 					Writer out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
 					StringBuilder sb = new StringBuilder();
 					Date now = new Date();
-					/*int c;
-					while((c = in.read()) != -1){
-						sb.append((char) c);
+					//int c;
+					//while((c = in.read()) != -1){
+					//	sb.append((char) c);
 						//System.out.println((char)c);
-					}*/
+					//}
 					String fragment;
 					while((fragment = in.readLine()) != null){
 						System.out.println("Some enytry...");
@@ -52,19 +134,12 @@ public class ChatServer{
 						out.flush();
 					}
 					System.out.println("ee");
-					
-					//System.out.println(sb.toString()+", "+now.toString());
-					/*Writer out = new OutputStreamWriter(connection.getOutputStream());
-					Date now = new Date();
-					out.write(now.toString() + "\r\n");
-					out.flush();
-					connection.close();
-					*/
 				} catch(IOException ex){}
 			}
 		} catch(IOException ex){
 			System.err.println(ex);
 		}
+		*/
 	}
 
 	private static class MainLoginThread extends Thread {
